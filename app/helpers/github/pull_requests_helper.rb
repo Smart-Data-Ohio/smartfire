ee983e91af100fc7f7f1163e7af312bce07dcde0
Riel St. Amand <riel.stamand@gmail.com>
2026-09-22 15:28:24 -0400

    Render PR cards when the queue is down

--- app/helpers/github/pull_requests_helper.rb
+++ app/helpers/github/pull_requests_helper.rb
@@ -142,7 +142,13 @@ module Github::PullRequestsHelper
     def request_pr_refresh(pull_request)
       return unless pull_request.stale?
       return unless (@github_pr_fetches ||= Set.new).add?(pull_request.id)
+      return unless pull_request.claim_fetch_request!
 
-      Github::FetchPullRequestJob.perform_later(pull_request) if pull_request.claim_fetch_request!
+      begin
+        Github::FetchPullRequestJob.perform_later(pull_request)
+      rescue Redis::BaseError, RedisClient::Error => error
+        # The queue is down: serve the stale card instead of breaking the page.
+        Rails.logger.warn "Skipping PR refresh enqueue for #{pull_request.id}: #{error.class}"
+      end
     end
 end
