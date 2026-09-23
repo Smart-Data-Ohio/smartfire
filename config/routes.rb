374adc0de6fd232f6106d8bd613a37571df002b1
Riel St. Amand <riel.stamand@gmail.com>
2026-09-22 23:56:23 -0400

    Add admin integration health page

--- config/routes.rb
+++ config/routes.rb
@@ -44,6 +44,7 @@ Rails.application.routes.draw do
       resource :join_code, only: :create
       resource :logo, only: %i[ show destroy ]
       resource :custom_styles, only: %i[ edit update ]
+      resource :integrations_health, only: :show, controller: "integrations_health"
     end
   end
 
